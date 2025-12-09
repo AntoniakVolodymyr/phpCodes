657d26aeb10fee340b938575249cb928adcf90d2
AntoniakVolodymyr <antoniak.volodymyr@comp-sc.if.ua>
2025-12-09 20:24:39 +0200

    Update index.php

--- ArraysPHP/index.php
+++ ArraysPHP/index.php
@@ -26,7 +26,7 @@ foreach ($categories AS $category) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <link rel="stylesheet"/>
     <title>Document</title>
 </head>
 <body>
