b187c4cc437c8ac8d423ef0d602c657b651bdc36
AntoniakVolodymyr <antoniak.volodymyr@comp-sc.if.ua>
2025-12-10 02:10:46 +0200

    Add files via upload

---
+++ ProjectsFromCource/6 - Name Explorer/inc/db-connect.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+try {
+    $pdo = new PDO(
+        'mysql:host=127.0.0.1;port=3307;dbname=names;charset=utf8mb4',
+        'root',
+        '',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} 
+catch (PDOException $e) {
+    // var_dump($e->getMessage());
+    echo 'A problem occured with the database connection...';
+    die();
+}
+
+/*
+$stmt = $pdo->prepare('SELECT * FROM `names`');
+$stmt->execute();
+var_dump($stmt->fetch(PDO::FETCH_ASSOC));
+*/
---
+++ ProjectsFromCource/9 - Content Management System/inc/db-connect.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+try {
+    $pdo = new PDO(
+        'mysql:host=127.0.0.1;port=3307;dbname=cms;charset=utf8mb4',
+        'root',
+        '',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} 
+catch (PDOException $e) {
+    // var_dump($e->getMessage());
+    echo 'A problem occured with the database connection...';
+    die();
+}
+
+return $pdo;
